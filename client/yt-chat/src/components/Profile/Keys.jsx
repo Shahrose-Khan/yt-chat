daa3ff2c929955678453a3d49f81de9d4c697d15
Shahrose-Khan <shezee247@outlook.com>
2024-07-23 22:59:12 +0500

    Reduce Keys to 1

--- client/yt-chat/src/components/Profile/Keys.jsx
+++ client/yt-chat/src/components/Profile/Keys.jsx
@@ -6,8 +6,6 @@ function Keys() {
       <h1>Keys</h1>
       <div className="keys-text">
         <p className="keys-box">Key box 1</p>
-        <p className="keys-box">Key box 2</p>
-        <p className="keys-box">Key box 3</p>
       </div>
     </div>
   );
